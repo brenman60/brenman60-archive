9c6b7980eead678fedc750e51233b1db27d650f7
brenman60 <brennankplays@gmail.com>
2026-07-07 16:16:29 -0400

    Fix GitHub Pages not found

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,20 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import ArticlesProvider from "./contexts/ArticlesContext.jsx";
 
+// Replace window history so that React router uses correct path
+try {
+  const params = new URLSearchParams(window.location.search);
+  const p = params.get("p");
+  if (p) {
+    const decoded = decodeURIComponent(p);
+    if (decoded.startsWith("/")) {
+      window.history.replaceState({}, "", decoded);
+    }
+  }
+} catch (e) {
+  
+}
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ArticlesProvider>
